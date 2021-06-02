f917ec72cab69034b981b2f87013be5fcefa9bd0
sachira <sachiradilthushka@gmail.com>
2021-06-02 15:59:20 +0530

    navbar

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Navbar from "./components/navbar.component";
 import ExercisesList from "./components/exercise-list.component";
 import EditExercise from "./components/edit-exercise.component";
 import CreateExercise from "./components/create-exercise.component";
+import CreateUser from "./components/create-user.component"
 function App() {
   return (
   <Router>
---
+++ src/components/edit-exercise.component.jsx
@@ -0,0 +1,12 @@
+import React, {Component } from 'react';
+import { Link } from 'react-router-dom';
+
+
+export default class EditExercise extends Component {
+
+    render(){
+       return(
+           <h1>Edit Exercise</h1>
+       ); 
+    }
+}
